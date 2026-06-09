5b64ae37a5a625a68902d7b4e982b31a798f6b60
kayla4smr <rayk7622@student.faytechcc.edu>
2026-06-09 13:35:22 -0400

    Add files via upload

---
+++ P1LAB.py
@@ -0,0 +1,9 @@
+# Kayla Ray
+# 06/09/2026
+# P1LAB1
+# this program asks for the user's name and then greets them
+
+firstName = input(" What is your first name? ")
+lastName = input(" what is your last name? ")
+print(f"Hello, {firstName} {lastName}! Welcome to CTI-110.")
+
